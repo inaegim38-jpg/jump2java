17ba05f9293e833e52476a35b503a56ff6d1152a
ãinaegim38-jpg <inaegim38@gmail.com>
2025-08-29 11:38:47 +0900

    설정파일 추가

---
+++ src/CalManage.java
@@ -0,0 +1,11 @@
+public class CalManage {
+    public static void mai(String[] args) {
+        Calculator cal = Calculator();
+        cal.add(10);
+        System.out.println(cal.result);
+        cal.add(100);
+        System.out.println(cal.result);
+        cal2.add(1000);
+        System.out.println(cal2.result);
+    }
+}
---
+++ src/Calculator.java
@@ -0,0 +1,12 @@
+int result = 0;
+int add(int num){
+    result += num;
+    return result;
+}
+}
+class Sampple{
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.add(100))gi;
+    }
+}
